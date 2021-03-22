eaff02f75b3eaf3b589662420372a98d243b6e85
Dan <deweerdd@gmail.com>
2021-03-21 22:28:43 -0700

    Refactor to components

--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from './Header'
+import Content from './Content'
+import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -13,16 +15,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
