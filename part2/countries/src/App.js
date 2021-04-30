16354a708123c1e959c443d6e1c19a2037c4dbb6
Dan <deweerdd@gmail.com>
2021-04-30 08:15:06 -0700

    Load Countries on first render

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,8 +1,28 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
+const App = () => {
 
-function App() {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+  useEffect(() => {
+    axios
+    .get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      setCountries(response.data)
+    })
+  }, [])
+
+  console.log(countries)
+
   return (
-    <div>
+    <div>find countries: 
+      <input onChange={handleChange}></input>
     </div>
   );
 }
