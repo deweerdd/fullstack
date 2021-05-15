4e250b0cef2a945d38633ab14f5592dff6a1dbe1
Dan DeWeerd <deweerdd@gmail.com>
2021-05-15 13:51:58 -0700

    3.6 Exercises Complete Error Handling

--- part2/phonebook-be/index.js
+++ part2/phonebook-be/index.js
@@ -1,8 +1,10 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
+app.use(express.json())
 const port = 3001;
 
-const persons =
+let persons =
     [
         {
             id: 1,
@@ -41,6 +43,59 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.send(persons);
+})
+
+function getRandomId(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
+
+let idExists = (id) => {
+    if (persons.find(p => p.id === id)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    if (!idExists(id)) {
+        return response.status(404).send(`ID: ${id} not found in database`);
+    }
+    response.send(persons.find(p => p.id === id));
+})
+app.post('/api/persons', (request, response) => {
+    let body = request.body;
+    console.log(body.name, body.number);
+
+    if (body.name === undefined) {
+        return response.status(404).send(`Error: name is ${body.name}`);
+    }
+
+    if (body.number === undefined) {
+        return response.status(404).send(`Error: number is ${body.number}`);
+    }
+
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(500).send(`Error: ${body.name} already exists`);
+    }
+
+    let person = {
+        id: getRandomId(1, 1000),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person);
+    response.status(200).send(persons);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    if (!idExists(id)) {
+        return response.status(404).send(`ID: ${id} not found in database`);
+    }
+    persons = persons.filter(p => p.id !== id);
+    response.status(204).send(persons);
 })
 
 app.listen(port, () => {
